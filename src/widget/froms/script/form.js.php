f8f110d1b46b39c79e9fe41717fb957d263ad941
Crazy <c919342864@yahoo.com>
2017-05-16 15:45:15 +0800

    update

--- src/widget/froms/script/form.js.php
+++ src/widget/froms/script/form.js.php
@@ -1,22 +1,38 @@
 $('form[name="form"]').submit(function () {
 
-    $.toast({
-        heading: '提示',
-        text: '操作成功,正在跳转',
-        position: 'top-center',
-        icon: 'success',
-        hideAfter: 1000,
-        afterHidden: function () {
-            //location.href=location.href;
-        }
-    });
+
     console.log($(form).serializeArray());
+
     $.ajax({
         type: "<?= self::$way ?>",
         url: "<?= self::$url ?>",
         data: $(form).serializeArray(),
         success: function (event) {
-            console.log(event);
+            event = $.parseJSON(event);
+
+            if (event['code'] == 200) {
+                $.toast({
+                    heading: '提示',
+                    text: event['message'],
+                    position: 'top-center',
+                    icon: 'success',
+                    hideAfter: 1000,
+                    afterHidden: function () {
+                        //location.href=location.href;
+                    }
+                });
+            } else {
+                $.toast({
+                    heading: '提示',
+                    text: event['message'],
+                    position: 'top-center',
+                    icon: 'error',
+                    hideAfter: 1000,
+                    afterHidden: function () {
+                        //location.href=location.href;
+                    }
+                });
+            }
         }
     });
 });
